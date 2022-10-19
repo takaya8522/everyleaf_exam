76def4e5f98adadf11a1d28de8cc259ca85b92ee
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-19 20:43:09 +0900

    検索機能・ソート機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_18_113015) do
+ActiveRecord::Schema.define(version: 2022_10_18_155659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_10_18_113015) do
     t.date "deadline_on", null: false
     t.integer "priority", null: false
     t.integer "status", null: false
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
