eeab031befaa3492e94c6a20cb16940aa0a368a5
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-20 00:39:39 +0900

    ソート機能を昇順へ修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,7 @@ class Task < ApplicationRecord
   enum priority: { 低: 0, 中: 1, 高: 2 }
   enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
   scope :sort_created_at, -> { order("created_at DESC") }
-  scope :sort_deadline_on, -> { order("deadline_on DESC") }
+  scope :sort_deadline_on, -> { order("deadline_on") }
   scope :sort_priority, -> { order("priority DESC") }
   scope :search_status, ->(status) { where(status: status) }
   scope :search_title, ->(title) { where('title LIKE ?',"%#{title}%") }
