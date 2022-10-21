96e33ba7c11248b28e319ee02208b8fd60333d64
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-22 01:34:29 +0900

    ログイン機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
 
   def new
   end
@@ -6,11 +7,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+      log_in(user)
       redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:show]
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -6,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       redirect_to user_path(@user.id)
     else
       render :new
@@ -20,5 +24,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to current_user unless current_user?(@user)
   end
 end
