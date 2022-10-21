79cb9eb7a0a82da36ec4989cbca9949b61376131
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-22 00:23:02 +0900

    sessions/usersコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
+  validates :password, length: { minimum: 6 }
 end
