38e47a42127f9e981e94af4bcd18aa8ae36360f2
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-24 10:33:14 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
-10.times do |i|
-  Task.create!(title: "first_task", content: "テスト", deadline_on: "2025/02/18", priority: 1, status: 0)
+User.create!(name: "test", email: "test0001@example.com, password_digest: 123456, admin: true")
+User.create!(name: "test1", email: "test0002@example.com, password_digest: 123456, admin: false")
+
+50.times do |i|
+  Task.create!(title: "first_task", content: "テスト", deadline_on: "2025/02/18", priority: 1, status: 0, user_id: 1)
 end
 
-10.times do |i|
-  Task.create!(title: "second_task", content: "テスト", deadline_on: "2025/02/17", priority: 2, status: 1)
-end
-
-10.times do |i|
-  Task.create!(title: "third_task", content: "テスト", deadline_on: "2025/02/16", priority: 0, status: 2)
+50.times do |i|
+  Task.create!(title: "second_task", content: "テスト", deadline_on: "2025/02/17", priority: 2, status: 1, user_id: 2)
 end
