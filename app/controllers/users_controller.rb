f98dbb84b9cf1bf182ea78b56cae18d7ef3b1ff9
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-24 14:46:18 +0900

    html id属性修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
-      redirect_to user_path(@user.id)
+      redirect_to tasks_path
     else
       render :new
     end
