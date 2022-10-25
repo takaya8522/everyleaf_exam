f740cfb7649e1fee933431b0e02507a5e1423783
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-25 23:35:41 +0900

    label/userのアソシエーション実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+  has_and_belongs_to_many :users
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_25_114102) do
+ActiveRecord::Schema.define(version: 2022_10_25_141703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "labels_users", force: :cascade do |t|
+    t.bigint "label_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["label_id"], name: "index_labels_users_on_label_id"
+    t.index ["user_id"], name: "index_labels_users_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
@@ -38,5 +51,7 @@ ActiveRecord::Schema.define(version: 2022_10_25_114102) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "labels_users", "labels"
+  add_foreign_key "labels_users", "users"
   add_foreign_key "tasks", "users"
 end
