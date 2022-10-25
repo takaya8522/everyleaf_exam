30200333fd7c7a5ce24ba58c868b8c5220d59f78
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-25 18:41:53 +0900

    seeds内容修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-2.times do
-  User.create!(name: "adminsさん", email: "adminadmino@piyopiyo.com, password: 123456, password_confirmation: 123456, admin: true")
+1.times do
+  User.create!(name: "adminsさん", email: "adminadmino@piyopiyo.com", password: "123456", password_confirmation: "123456", admin: true)
 end
 
-2.times do
-  User.create!(name: "normalさん", email: "normalnormal@piyopiyo.com, password: 123456, password_confirmation: 123456, admin: false")
-end
-
-50.times do
-  Task.create!(title: "first_task", content: "テスト", deadline_on: "2025/02/18", priority: 1, status: 0, user_id: 1)
+1.times do
+  User.create!(name: "normalさん", email: "normalnormal@piyopiyo.com", password: "123456", password_confirmation: "123456", admin: false)
 end
 
 50.times do
-  Task.create!(title: "second_task", content: "テスト", deadline_on: "2025/02/17", priority: 2, status: 1, user_id: 2)
+  Task.create!(title: "first_task", content: "テスト", deadline_on: "2025/02/18", priority: 1, status: 0, user_id: User.first.id)
+end
+
+50.times do
+  Task.create!(title: "second_task", content: "テスト", deadline_on: "2025/02/17", priority: 2, status: 1, user_id: User.second.id)
 end
