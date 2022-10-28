0f4f43a1af1e2f461f00724e1f3adaa4d729c4d0
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-29 01:54:09 +0900

    エラーページ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :login_required, :logout_required
 
-  # エラー画面編集用
-  # protect_from_forgery with: :exception
-  # rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  # rescue_from ActionController::RoutingError, with: :render_404
-  # rescue_from Exception, with: :render_500
+  エラー画面編集用
+  protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
 
   # エラー画面編集用
   def render_404(exception = nil)
